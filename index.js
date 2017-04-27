d975af242a563235266912abad656b995f2bd34c
Christoph Bähr <christoph.baehr@gmx.de>
2017-04-27 23:27:08 +0200

    using user model controller

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ const fileUpload = require('express-fileupload');
 const multer = require('multer');
 const fs = require('fs');
 const app = express();
-const mongoose = require('mongoose');
+
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const config = require('./lib/config');
-const User = require('./lib/models/user');
-const bcrypt = require('bcrypt');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -21,15 +17,12 @@ app.use(bodyParser.json());
 app.use(fileUpload());
 app.use(cors());
 
-app.set('superSecret', config.secret);
-
 const seneca = require('seneca')()
 seneca.use('./lib/process-info.js', { seneca: seneca, __rootdir: __dirname });
 seneca.use('./lib/converter.js', { seneca: seneca, __rootdir: __dirname });
 seneca.use('./lib/file-info.js', { seneca: seneca, __rootdir: __dirname });
-
+seneca.use('./lib/user.js', { seneca: seneca, __rootdir: __dirname });
 const port = process.env.PORT || 3000;
-mongoose.connect(config.database);
 
 
 app.get('/videos/:filename', (req, res) => {
@@ -58,76 +51,35 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage }).fields(['files', 'poster']);
 
 app.post('/register', (req, res) => {
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
-
-    const user = new User({
-        name: req.body.name,
-        password: hash,
-        creator: req.body.isCreator,
-        gravatarUrl: req.body.gravatar
-    });
-
-    console.log(user);
-
-    user.save((error) => {
+    console.log(req.body.userName, req.body.password);
+    seneca.act({ user: 'register', userName: req.body.userName, password: req.body.password, isCreator: req.body.isCreator, avatarUrl: req.body.avatarUrl }, (error, user) => {
+        console.log(error, user);
         res.status(201).send(user).end();
     });
-
 });
 
-app.get('/users', (req, res) => {
-    User.find({}, (error, users) => {
-        res.send(users).status(204).end();
-    });
-});
+
 
 app.get('/user', (req, res) => {
 
     const token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-    jwt.verify(token, app.get('superSecret'), (error, user) => {
+    seneca.act({ user: 'verify', token: token }, (error, user) => {
         if (!error) {
-            res.status(200).send({
-                name: user.name,
-            creator: user.creator,
-        gravatar: user.gravatarUrl}).end();
+            res.status(200).send(user).end();
         } else {
-            res.status(401).end();
+            res.status(401).send(error.message).end();
         }
     });
 
 });
 
 app.post('/login', (req, res) => {
-    User.findOne({ name: req.body.name }, (error, user) => {
-
-        if (error) throw error;
-
-        if (!user) {
-            res.json({ success: false, message: 'Authentication failed. User not found.' });
-        } else if (user) {
-
-            // check if password matches
-
-            if (!bcrypt.compareSync(req.body.password, user.password)) {
-                res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-            } else {
-
-                // if user is found and password is right
-                // create a token
-                var token = jwt.sign(user.toJSON(), app.get('superSecret'), {
-                    expiresIn: 60 * 60 * 24
-                });
-
-                // return the information including token as JSON
-                res.json({
-                    success: true,
-                    message: 'Enjoy your token!',
-                    token: token
-                });
-            }
-
+    seneca.act({ user: 'login', userName: req.body.userName, password: req.body.password }, (error, token) => {
+        if (!error) {
+            res.status(200).send(token).end();
+        } else {
+            res.status(401).send(error).end();
         }
     });
 });
@@ -149,6 +101,16 @@ app.get('/files/', (req, res) => {
     });
 });
 
+app.get('/files/watch/:filename', (req, res) => {
+    seneca.act({ info: 'file', processId: req.params['filename'] }, (error, file) => {
+        if (!error) {
+            return res.status(200).send(file).end();
+        } else {
+            return res.status(404).end();
+        }
+    });
+});
+
 app.get('/upload/status/:processId', (req, res) => {
 
     seneca.act({ info: 'file', processId: req.params['processId'] }, (error, file) => {
@@ -164,60 +126,65 @@ app.get('/upload/status/:processId', (req, res) => {
  * FIXME: serious need in refactoring as this route handler is way to big and should delegate most actions to the microservices
  */
 app.post('/upload', (req, res) => {
-    if (!req.files) {
-        res.status(402).send('no files for upload sent').end();
-    }
-    const file = req.files.files;
-    const poster = req.files.poster;
-    const title = req.body.title;
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    seneca.act({ user: 'verify', token: token }, (error, user) => {
+        if (!user.isCreator) {
+            return res.status(401).send('You are not a creator').end();
+        }
+        if (!req.files) {
+            res.status(402).send('no files for upload sent').end();
+        }
+        const file = req.files.files;
+        const poster = req.files.poster;
+        const title = req.body.title;
 
-    upload(req, res, (error) => {
+        upload(req, res, (error) => {
 
-        if (!error) {
-            let destinationType = '';
-            let type = '';
-            let fileName = '';
-            if (file.mimetype.startsWith('image')) {
-                destinationType = 'files';
-                type = 'image';
-                fileName = file.name;
+            if (!error) {
+                let destinationType = '';
+                let type = '';
+                let fileName = '';
+                if (file.mimetype.startsWith('image')) {
+                    destinationType = 'files';
+                    type = 'image';
+                    fileName = file.name;
+
+                } else {
+                    destinationType = 'videos';
+                    type = 'video';
+                    fileName = file.name + '.temp';
+                }
+                file.mv(path.join(__dirname, 'uploads', fileName), (err) => {
+
+                    if (type === 'video') {
+                        seneca.act({ convert: 'video', fileName: fileName, userName: user.userName }, (error, data) => {
+                            const newFilename = data.fileName;
+                            const processId = data.processId;
+                            poster.mv(path.join(__dirname, 'uploads', poster.name), (err) => {
+                                seneca.act({
+                                    update: 'file',
+                                    file: path.join(destinationType, newFilename),
+                                    title: title,
+                                    type: type,
+                                    posterFile: path.join('files', poster.name),
+                                    processId: processId
+                                }, (error, result) => {
+                                    res.send(result).status(201).end();
+                                });
+                            })
+
+                        });
+                    } else {
+                    }
+                });
+
 
             } else {
-                destinationType = 'videos';
-                type = 'video';
-                fileName = file.name + '.temp';
+                console.error(error);
             }
-            file.mv(path.join(__dirname, 'uploads', fileName), (err) => {
+        });
 
-                if (type === 'video') {
-                    seneca.act({ convert: 'video', fileName: fileName }, (error, data) => {
-                        const newFilename = data.fileName;
-                        const processId = data.processId;
-                        poster.mv(path.join(__dirname, 'uploads', poster.name), (err) => {
-                            seneca.act({
-                                update: 'file',
-                                file: path.join(destinationType, newFilename),
-                                title: title,
-                                type: type,
-                                posterFile: path.join('files', poster.name),
-                                processId: processId
-                            }, (error, result) => {
-                                res.send(result).status(201).end();
-                            });
-                        })
-
-                    });
-                } else {
-                }
-            });
-
-
-        } else {
-            console.error(error);
-        }
     });
-
-
 
 });
 

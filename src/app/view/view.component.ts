6db444577d3ab47dda5c1322f146ada12da9fb88
Christoph Bähr <christoph.baehr@live.com>
2017-04-28 13:06:15 +0200

    overwrite native-video controls
    it looks great

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -19,9 +19,14 @@ export interface Record {
 export class ViewComponent implements AfterViewInit {
   public file: Record;
 
-  constructor(private activatedRoute: ActivatedRoute, private http: Http) { }
+  private interval: any;
+  private timeCode: String = '0:00';
+
+  constructor(private activatedRoute: ActivatedRoute, private http: Http) {
+  }
 
   ngAfterViewInit() {
+
     this.activatedRoute.params.subscribe((routeInfo: { id: string }) => {
       this.http.get('http://localhost:3000/files/watch/' + routeInfo.id).subscribe((file) => {
         this.file = file.json();
@@ -35,6 +40,55 @@ export class ViewComponent implements AfterViewInit {
 
   getPosterFileSrc(file:Record) {
     return 'http://localhost:3000/' + file.posterFilePath;
+  }
+
+  // media controls
+
+  goFullscreen() {
+      const video = document.querySelector('#video');
+      video.requestFullscreen();
+  }
+
+  isPaused() {
+    const video = document.querySelector('#video');
+    return (<any>video).paused;
+  }
+
+  calculateTimeCode() {
+    const video = <any>document.querySelector('#video');
+    const currentTime = video.currentTime;
+    let cT = Math.floor(currentTime % 60);
+    let secString = cT <= 9 ? '0' + cT : cT;
+
+    this.timeCode = Math.floor(currentTime / 60) + ":" + secString;
+  }
+
+  playOrPause() {
+      const video = document.querySelector('#video');
+      if ( this.isPaused() ) {
+        (<any>video).play();
+
+        this.interval = setInterval( () => {
+          this.calculateTimeCode();
+        }, 200);
+      } else {
+        (<any>video).pause();
+        if ( this.interval ) {
+          clearInterval(this.interval);
+        }
+      }
+  }
+
+  skipPrevious() {
+    const video = <any>document.querySelector('#video');
+    video.currentTime -= 10;
+    this.calculateTimeCode();
+  }
+
+  skipNext() {
+    const video = <any>document.querySelector('#video');
+    video.currentTime += 10;
+    this.calculateTimeCode();
   }
 
 }
